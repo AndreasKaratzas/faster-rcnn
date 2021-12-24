2a34346d3508bb743aa0180e971e38aa4d5c1374
Andreas Karatzas <andreas.karatzas@protonmail.com>
2021-12-25 00:09:06 +0200

    Upgraded coco dataset preprocessing.

--- lib/coco2rcnn.py
+++ lib/coco2rcnn.py
@@ -1,14 +1,14 @@
 
-import json
-import os
-import shutil
-import zipfile
-from pathlib import Path
-from typing import List
-
-import numpy as np
-import wget
 from tqdm import tqdm
+import numpy as np
+from typing import List
+from pathlib import Path
+from PIL import Image
+import zipfile
+import shutil
+import os
+import json
+import wget
 
 
 class installCocoDataset:
@@ -42,16 +42,12 @@ class installCocoDataset:
     def extract(self):
         for filepath in self.filepaths:
             with zipfile.ZipFile(filepath) as zf:
-                for member in tqdm(zf.infolist(), desc="Extracting", unit=" files/sec"):
+                for member in tqdm(zf.infolist(), desc="Extracting", unit=" files"):
                     try:
                         zf.extract(member, str(
                             Path(self.res_dir) / Path(filepath).stem))
                     except zipfile.error as e:
                         pass
-
-    def convert(self, sample):
-        x1, y1, w, h = sample['bbox']
-        return x1, y1, x1 + w, y1 + h
 
     def clean(self):
         filenames = os.listdir(os.path.join(
@@ -76,190 +72,162 @@ class installCocoDataset:
             shutil.move(os.path.join(self.res_dir, "val2017", "val2017", filename), os.path.join(
                 self.res_dir, "valid", "images", filename))
 
-        shutil.rmtree(os.path.join(self.res_dir, "annotations_trainval2017"))
         shutil.rmtree(os.path.join(self.res_dir, "train2017"))
         shutil.rmtree(os.path.join(self.res_dir, "val2017"))
 
-        os.remove(os.path.join(self.res_dir, "annotations_trainval2017.zip"))
         os.remove(os.path.join(self.res_dir, "train2017.zip"))
         os.remove(os.path.join(self.res_dir, "val2017.zip"))
 
+    def rotate(self, image):
+        rot_flag = False
+        exif = image.getexif()
+        orientation = exif.get(0x0112, 1)  # default 1
+        if orientation > 1:
+            method = {
+                2: Image.FLIP_LEFT_RIGHT,
+                3: Image.ROTATE_180,
+                4: Image.FLIP_TOP_BOTTOM,
+                5: Image.TRANSPOSE,
+                6: Image.ROTATE_270,
+                7: Image.TRANSVERSE,
+                8: Image.ROTATE_90,
+            }.get(orientation)
+            if method is not None:
+                rot_flag = True
+                image = image.transpose(method)
+                del exif[0x0112]
+                image.info["exif"] = exif.tobytes()
+        return rot_flag, image
+
     def preprocess(self):
-        filenames = os.listdir(os.path.join(
-            self.res_dir, "train", "images"))
 
-        for filename in tqdm(
-            iterable=filenames,
-            desc="Removing empty training samples",
-            unit=" samples/sec"
-        ):
-            if os.stat(os.path.join(self.res_dir, "train", "images", filename)).st_size == 0:
-                os.remove(os.path.join(self.res_dir,
-                          "train", "images", filename))
+        subsets = ['train', 'valid']
 
-        filenames = os.listdir(os.path.join(
-            self.res_dir, "valid", "images"))
-
-        for filename in tqdm(
-            iterable=filenames,
-            desc="Removing empty validation samples",
-            unit=" samples/sec"
-        ):
-            if os.stat(os.path.join(self.res_dir, "valid", "images", filename)).st_size == 0:
-                os.remove(os.path.join(self.res_dir,
-                          "valid", "images", filename))
-
-        train_image_dir = Path(self.res_dir) / "train" / "images"
-        train_label_dir = Path(self.res_dir) / "train" / "labels"
-        valid_image_dir = Path(self.res_dir) / "valid" / "images"
-        valid_label_dir = Path(self.res_dir) / "valid" / "labels"
-
-        train_images = Path(train_image_dir).glob('*.*')
-        train_labels = Path(train_label_dir).glob('*.txt')
-        valid_images = Path(valid_image_dir).glob('*.*')
-        valid_labels = Path(valid_label_dir).glob('*.txt')
-
-        train_img_filenames = [f.stem for f in train_images]
-        train_lbl_filenames = [f.stem for f in train_labels]
-        valid_img_filenames = [f.stem for f in valid_images]
-        valid_lbl_filenames = [f.stem for f in valid_labels]
-
-        train_img_sup = list(set(train_img_filenames) -
-                             set(train_lbl_filenames))
-        valid_img_sup = list(set(valid_img_filenames) -
-                             set(valid_lbl_filenames))
-        train_lbl_sup = list(set(train_lbl_filenames) -
-                             set(train_img_filenames))
-        valid_lbl_sup = list(set(valid_lbl_filenames) -
-                             set(valid_img_filenames))
-
-        if train_img_sup:
-            train_images = os.listdir(train_image_dir)
-
-            for train_img in tqdm(
-                iterable=train_img_sup,
-                desc="Removing redundant training image samples",
-                unit=" samples/sec"
+        for subset_id in subsets:
+            rot_cntr = 0
+            file_not_found_cntr = 0
+            imgs_lst = os.listdir(os.path.join(
+                self.res_dir, subset_id, "images"))
+            for img_path in tqdm(
+                iterable=imgs_lst,
+                desc=f"Parsing {subset_id} image subfolders",
+                total=len(imgs_lst),
+                unit=" samples"
             ):
-                res = list(filter(lambda x: train_img in x, train_images))
-                os.remove(os.path.join(self.res_dir,
-                                       "train", "images", res[0]))
 
-        if valid_img_sup:
-            valid_images = os.listdir(valid_image_dir)
+                try:
+                    label = np.loadtxt(
+                        os.path.join(
+                            self.res_dir, 
+                            subset_id, "label", 
+                            str(Path(img_path).resolve().stem) + ".txt"
+                        )
+                    )
+                    
+                    img = Image.open(os.path.join(
+                        self.res_dir, subset_id, "images", img_path))
+                    rotation_flag, img = self.rotate(img)
 
-            for valid_img in tqdm(
-                iterable=valid_img_sup,
-                desc="Removing redundant validation image samples",
-                unit=" samples/sec"
+                    if rotation_flag:
+                        rot_cntr += 1
+                        os.remove(os.path.join(
+                            self.res_dir, subset_id, "images", img_path))
+                        img.save(os.path.join(
+                            self.res_dir, subset_id, "images", img_path))
+                
+                except IOError:
+
+                    file_not_found_cntr += 1
+
+            if rot_cntr > 0:
+                print(f"Number of images rotated: {rot_cntr}.\n")
+            if file_not_found_cntr > 0:
+                os.remove(os.path.join(
+                    self.res_dir, subset_id, "images", img_path))
+                print(f"Deleted {file_not_found_cntr} images.\n")
+
+    def install(self):
+        subsets = ['train', 'valid']
+        original = ['train2017', 'val2017']
+        
+        cntr = 0
+
+        for origin, subset_id in zip(original, subsets):
+
+            imgs_paths = os.listdir(os.path.join(
+                self.res_dir, subset_id, "images"))
+
+            for img_path in tqdm(
+                iterable=imgs_paths,
+                desc=f"Parsing {subset_id} subset for samples",
+                total=len(imgs_paths),
+                unit=" samples"
             ):
-                res = list(filter(lambda x: valid_img in x, valid_images))
-                os.remove(os.path.join(self.res_dir,
-                                       "valid", "images", res[0]))
+                img = Image.open(os.path.join(
+                    self.res_dir, subset_id, "images", img_path))
+                width, height = img.size
 
-        if train_lbl_sup:
-            train_labels = os.listdir(train_label_dir)
-            train_labels = [
-                fname for fname in train_labels if fname.endswith('.txt')]
+                try:
+                    label = np.loadtxt(os.path.join(
+                        self.res_dir,
+                        "coco2017labels",
+                        "coco", "labels", origin,
+                        str(Path(img_path).resolve().stem) + '.txt')
+                    )
 
-            for train_lbl in tqdm(
-                iterable=train_lbl_sup,
-                desc="Removing redundant training label samples",
-                unit=" samples/sec"
-            ):
-                res = list(filter(lambda x: train_lbl in x, train_labels))
-                os.remove(os.path.join(self.res_dir,
-                                       "train", "labels", res[0]))
+                    if label.size != 0:
+                        if label.ndim < 2:
+                            label = label.reshape(1, -1)
 
-        if valid_lbl_sup:
-            valid_labels = os.listdir(valid_label_dir)
-            valid_labels = [
-                fname for fname in valid_labels if fname.endswith('.txt')]
+                        classes = np.array(label[:, 0]).astype(int)
+                        x_center = np.array(label[:, 1] * width).reshape(1, -1)
+                        y_center = np.array(
+                            label[:, 2] * height).reshape(1, -1)
+                        obj_width = np.array(
+                            (label[:, 3] * width)).reshape(1, -1)
+                        obj_height = np.array(
+                            (label[:, 4] * height)).reshape(1, -1)
 
-            for valid_lbl in tqdm(
-                iterable=valid_lbl_sup,
-                desc="Removing redundant validation label samples",
-                unit=" samples/sec"
-            ):
-                res = list(filter(lambda x: valid_lbl in x, valid_labels))
-                os.remove(os.path.join(self.res_dir,
-                                       "valid", "labels", res[0]))
+                        x1 = x_center - (obj_width / 2)
+                        y1 = y_center - (obj_height / 2)
+                        x2 = x_center + (obj_width / 2)
+                        y2 = y_center + (obj_height / 2)
 
-    def install(self, annotation_filepath: str, out_filepath):
+                        annotation_bbox = np.ceil(
+                            np.vstack((x1, y1, x2, y2)).T)
 
-        dataset_filepath = open(annotation_filepath)
-        dataset_data = json.load(dataset_filepath)
+                        annotation_sample = np.column_stack(
+                            (classes.astype(str), annotation_bbox.astype(str)))
 
-        annotations = []
+                        np.savetxt(
+                            fname=os.path.join(
+                                self.res_dir, subset_id, "labels", str(Path(img_path).resolve().stem) + '.txt'),
+                            X=annotation_sample,
+                            fmt='%s')
 
-        for annotation in tqdm(
-            iterable=dataset_data['annotations'],
-            desc="Converting COCO dataset to Faster R-CNN format",
-            unit=" labels/sec"
-        ):
-            image_id = annotation['image_id']
-
-            if self.init_flag:
-                self.prev_image_id = image_id
-                self.init_flag = False
-
-            x1, y1, x2, y2 = self.convert(annotation)
-            annotation_bbox = np.ceil(np.array([x1, y1, x2, y2]))
-            annotation_id = np.array([annotation['category_id']])
-            annotation_sample = np.append(
-                annotation_id.astype(str), annotation_bbox.astype(str))
-
-            if self.prev_image_id == image_id:
-                annotations.append(annotation_sample)
+                except IOError:
+                    cntr += 1
+            
+            if cntr > 0:
+                print(f"Number of files not found: {cntr}.\n")
             else:
-                np.savetxt(os.path.join(
-                    out_filepath, str(self.prev_image_id).zfill(12) + '.txt'), annotations, fmt='%s')
-                annotations = []
-                annotations.append(annotation_sample)
-                self.prev_image_id = image_id
+                print(f"All files were found.")
 
-        dataset_filepath.close()
+        shutil.rmtree(os.path.join(self.res_dir, "coco2017labels"))
+        os.remove(os.path.join(self.res_dir, "coco2017labels.zip"))
 
     def driver(self):
         self.configure()
         self.download()
         self.extract()
-
-        self.install(
-            annotation_filepath=os.path.join(
-                self.res_dir,
-                'annotations_trainval2017',
-                'annotations',
-                'instances_train2017.json'
-            ),
-            out_filepath=os.path.join(
-                self.res_dir,
-                'train',
-                'labels'
-            )
-        )
-
-        self.install(
-            annotation_filepath=os.path.join(
-                self.res_dir,
-                'annotations_trainval2017',
-                'annotations',
-                'instances_val2017.json'
-            ),
-            out_filepath=os.path.join(
-                self.res_dir,
-                'valid',
-                'labels'
-            )
-        )
-
         self.clean()
+        self.install()
         self.preprocess()
 
 
 url_train_imgs = "http://images.cocodataset.org/zips/train2017.zip"
 url_valid_imgs = "http://images.cocodataset.org/zips/val2017.zip"
-url_lbls = "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+url_lbls = "https://github.com/ultralytics/yolov5/releases/download/v1.0/coco2017labels.zip"
 
 res_dir = Path("../datasets/coco")
 
